e163de91c1ae92905dcdc5b4dd33f06d4d51e9ba
Vinit Gupta <vinitman35@gmail.com>
2021-05-26 13:06:51 +0530

    add solution to diamond question

---
+++ diamond.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+    int n,i,j,k;
+
+    printf("Enter the limit : ");
+    scanf("%d",&n);
+    i=0;
+    while(i<n){
+        j=0;
+        while(j<n-i){
+            printf(" ");
+            j++;
+        }
+        k=0;
+        while(k<i*2+1){
+            printf("*");
+            k++;
+        }
+        printf("\n");
+        i++;
+    }
+    i=0;
+    while(i<n-1){
+        j=0;
+        while(j<=i+1){
+            printf(" ");
+            j++;
+        }
+        k=i;
+        while(k<=n-i){
+            printf("*");
+            k++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
+
+/**
+ *0      @
+ *1    @ @ @
+ *2  @ @ @ @ @
+ *3@ @ @ @ @ @ @
+  0  @ @ @ @ @
+ *1    @ @ @
+ *2      @
+ * 
+ */
